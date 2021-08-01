5796bb0393a72c03467583adbaf92b178c4d425a
shafafathima <87116799+shafafathima@users.noreply.github.com>
2021-08-01 22:34:13 +0530

    Add files via upload

---
+++ LabWeek3/Exercise14.java
@@ -0,0 +1,10 @@
+package knightingale;
+public class Exercise15 {
+//	public static void main(String[] args) {
+//	public void main(String [] args) {
+//	static void main(String [] args) {
+	public static void main() {
+		
+	}
+		
+	}
